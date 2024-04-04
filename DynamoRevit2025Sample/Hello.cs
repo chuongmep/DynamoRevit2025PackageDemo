de661b4233b2e77759cbfb7a38598b33eeb30493
chuongmep <31106432+chuongmep@users.noreply.github.com>
2024-04-04 22:37:03 +0800

    init dynamo revit 2025 example

---
+++ DynamoRevit2025Sample/Hello.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.Revit.DB;
+using RevitServices.Persistence;
+
+namespace DynamoRevit2025Sample;
+
+public static class Hello
+{
+    public static string SayHello(string name)
+    {
+        return $"Hello, {name}!";
+    }
+    public static double Add(double a, double b)
+    {
+        return a + b;
+    }
+    public static Document GetDBDocument()
+    {
+         var doc = DocumentManager.Instance.CurrentDBDocument;
+         return doc;
+    }
+}
